a009c7996db4459b6440ca552366ae4a40902099
Jeffrey Stedfast <fejj@gnome.org>
2013-09-10 07:53:39 -0400

    Implemented a Unix->DOS new-line filter

---
+++ MimeKit/Filters/Unix2DosFilter.cs
@@ -0,0 +1,82 @@
+//
+// Unix2DosFilter.cs
+//
+// Author: Jeffrey Stedfast <jeff@xamarin.com>
+//
+// Copyright (c) 2013 Jeffrey Stedfast
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+using System;
+
+namespace MimeKit.Filters {
+	public class Unix2DosFilter : MimeFilterBase
+	{
+		byte pc;
+
+		public Unix2DosFilter ()
+		{
+		}
+
+		unsafe int Filter (byte* inbuf, int length, byte* outbuf)
+		{
+			byte* inend = inbuf + length;
+			byte* outptr = outbuf;
+			byte* inptr = inbuf;
+
+			while (inptr < inend) {
+				if (*inptr == (byte) '\r') {
+					*outptr++ = *inptr;
+				} else if (*inptr == (byte) '\n') {
+					if (pc != (byte) '\r')
+						*outptr++ = (byte) '\r';
+					*outptr++ = *inptr;
+				} else {
+					*outptr++ = *inptr;
+				}
+
+				pc = *inptr++;
+			}
+
+			return (int) (outptr - outbuf);
+		}
+
+		protected override byte[] Filter (byte[] input, int startIndex, int length, out int outputIndex, out int outputLength, bool flush)
+		{
+			EnsureOutputSize (length * 2, false);
+
+			outputIndex = 0;
+
+			unsafe {
+				fixed (byte* inptr = input, outptr = output) {
+					outputLength = Filter (inptr + startIndex, length, outptr);
+				}
+			}
+
+			return output;
+		}
+
+		public override void Reset ()
+		{
+			pc = 0;
+			base.Reset ();
+		}
+	}
+}
