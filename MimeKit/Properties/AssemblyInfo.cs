2ed17e791bb735db3c8457cfb8198fbc0d24795f
Jeffrey Stedfast <Jeffrey Stedfast>
2013-09-28 12:49:30 -0400

    Updated AssemblyInfo.cs version information and comments about how to bump them when making releases.

--- MimeKit/Properties/AssemblyInfo.cs
+++ MimeKit/Properties/AssemblyInfo.cs
@@ -5,32 +5,49 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MimeKit")]
-[assembly: AssemblyDescription("MIME and S/MIME creation and parser library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MimeKit")]
-[assembly: AssemblyCopyright("Copyright © 2013 Jeffrey Stedfast")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle ("MimeKit")]
+[assembly: AssemblyDescription ("MIME and S/MIME creation and parser library")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("MimeKit")]
+[assembly: AssemblyCopyright ("Copyright © 2013 Jeffrey Stedfast")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible (false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2fe79b66-d107-45da-9493-175f59c4a53c")]
+[assembly: Guid ("2fe79b66-d107-45da-9493-175f59c4a53c")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
 //      Minor Version 
+//      Micro Version
 //      Build Number
-//      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//
+// Note: AssemblyVersion is what the CLR matches against at runtime, so be careful
+// about updating it. The AssemblyFileVersion is the official release version while
+// the AssemblyInformationalVersion is just used as a display version.
+//
+// Based on my current understanding, AsemblyVersion is essentially the "API Version"
+// and so should only be updated when the API changes. The other 2 Version attributes
+// represent the "Release Version".
+//
+// Making releases:
+//
+// If any classes, methods, or enum values have been added, bump the Micro Version
+//    in all version attributes and set the Build Number back to 0.
+//
+// If there have only been bug fixes, bump the Micro Version and/or the Build Number
+//    in the AssemblyFileVersion attribute.
+[assembly: AssemblyInformationalVersion ("0.5")]
+[assembly: AssemblyFileVersion ("0.5.0.0")]
+[assembly: AssemblyVersion ("0.5.0.0")]
