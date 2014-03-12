56eee63a81691d8ee5600905dc69534905d3d059
Jeffrey Stedfast <jeff@xamarin.com>
2014-03-12 09:45:16 -0400

    Added a new TemporarySecureMimeContext which does not persist certs/keys/crls
    Adds a feature requested in issue #38

--- MimeKit/Cryptography/TemporarySecureMimeContext.cs
+++ MimeKit/Cryptography/TemporarySecureMimeContext.cs
@@ -1,5 +1,5 @@
-//
-// DummySecureMimeContext.cs
+﻿//
+// TemporarySecureMimeContext.cs
 //
 // Author: Jeffrey Stedfast <jeff@xamarin.com>
 //
@@ -35,21 +35,45 @@ using Org.BouncyCastle.X509.Store;
 using Org.BouncyCastle.X509;
 
 using MimeKit;
-using MimeKit.Cryptography;
 
-namespace UnitTests {
-	public class DummySecureMimeContext : SecureMimeContext
+namespace MimeKit.Cryptography {
+	/// <summary>
+	/// An S/MIME context that does not persist certificates, private keys or CRLs.
+	/// </summary>
+	/// <remarks>
+	/// A <see cref="TemporarySecureMimeContext"/> is a special S/MIME context that
+	/// does not use a persistent store for certificates, private keys, or CRLs.
+	/// Instead, certificates, private keys, and CRLs are maintained in memory only.
+	/// </remarks>
+	public class TemporarySecureMimeContext : SecureMimeContext
 	{
-		readonly Dictionary<X509Certificate, EncryptionAlgorithm[]> capabilities = new Dictionary<X509Certificate, EncryptionAlgorithm[]> ();
-		internal readonly Dictionary<X509Certificate, AsymmetricKeyParameter> keys = new Dictionary<X509Certificate, AsymmetricKeyParameter> ();
-		internal readonly List<X509Certificate> certificates = new List<X509Certificate> ();
-		internal readonly List<X509Crl> crls = new List<X509Crl> ();
+		readonly Dictionary<X509Certificate, EncryptionAlgorithm[]> capabilities;
+		readonly Dictionary<X509Certificate, AsymmetricKeyParameter> keys;
+		readonly List<X509Certificate> certificates;
+		readonly List<X509Crl> crls;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MimeKit.Cryptography.TemporarySecureMimeContext"/> class.
+		/// </summary>
+		/// <remarks>
+		/// Creates a new <see cref="TemporarySecureMimeContext"/>.
+		/// </remarks>
+		public TemporarySecureMimeContext ()
+		{
+			capabilities = new Dictionary<X509Certificate, EncryptionAlgorithm[]> ();
+			keys = new Dictionary<X509Certificate, AsymmetricKeyParameter> ();
+			certificates = new List<X509Certificate> ();
+			crls = new List<X509Crl> ();
+		}
 
 		#region implemented abstract members of SecureMimeContext
 
 		/// <summary>
-		/// Gets the X.509 certificate based on the selector.
+		/// Gets the X.509 certificate matching the specified selector.
 		/// </summary>
+		/// <remarks>
+		/// Gets the first certificate that matches the specified selector.
+		/// </remarks>
 		/// <returns>The certificate on success; otherwise <c>null</c>.</returns>
 		/// <param name="selector">The search criteria for the certificate.</param>
 		protected override X509Certificate GetCertificate (IX509Selector selector)
@@ -66,8 +90,11 @@ namespace UnitTests {
 		}
 
 		/// <summary>
-		/// Gets the private key based on the provided selector.
+		/// Gets the private key for the certificate matching the specified selector.
 		/// </summary>
+		/// <remarks>
+		/// Gets the private key for the first certificate that matches the specified selector.
+		/// </remarks>
 		/// <returns>The private key on success; otherwise <c>null</c>.</returns>
 		/// <param name="selector">The search criteria for the private key.</param>
 		protected override AsymmetricKeyParameter GetPrivateKey (IX509Selector selector)
@@ -90,6 +117,10 @@ namespace UnitTests {
 		/// <summary>
 		/// Gets the trusted anchors.
 		/// </summary>
+		/// <remarks>
+		/// A trusted anchor is a trusted root-level X.509 certificate,
+		/// generally issued by a certificate authority (CA).
+		/// </remarks>
 		/// <returns>The trusted anchors.</returns>
 		protected override Org.BouncyCastle.Utilities.Collections.HashSet GetTrustedAnchors ()
 		{
@@ -105,6 +136,11 @@ namespace UnitTests {
 		/// <summary>
 		/// Gets the intermediate certificates.
 		/// </summary>
+		/// <remarks>
+		/// An intermediate certificate is any certificate that exists between the root
+		/// certificate issued by a Certificate Authority (CA) and the certificate at
+		/// the end of the chain.
+		/// </remarks>
 		/// <returns>The intermediate certificates.</returns>
 		protected override IX509Store GetIntermediateCertificates ()
 		{
@@ -120,6 +156,11 @@ namespace UnitTests {
 		/// <summary>
 		/// Gets the certificate revocation lists.
 		/// </summary>
+		/// <remarks>
+		/// A Certificate Revocation List (CRL) is a list of certificate serial numbers issued
+		/// by a particular Certificate Authority (CA) that have been revoked, either by the CA
+		/// itself or by the owner of the revoked certificate.
+		/// </remarks>
 		/// <returns>The certificate revocation lists.</returns>
 		protected override IX509Store GetCertificateRevocationLists ()
 		{
@@ -129,6 +170,13 @@ namespace UnitTests {
 		/// <summary>
 		/// Gets the <see cref="CmsRecipient"/> for the specified mailbox.
 		/// </summary>
+		/// <remarks>
+		/// <para>Constructs a <see cref="CmsRecipient"/> with the appropriate certificate and
+		/// <see cref="CmsRecipient.EncryptionAlgorithms"/> for the specified mailbox.</para>
+		/// <para>If the mailbox is a <see cref="SecureMailboxAddress"/>, the
+		/// <see cref="SecureMailboxAddress.Fingerprint"/> property will be used instead of
+		/// the mailbox address.</para>
+		/// </remarks>
 		/// <returns>A <see cref="CmsRecipient"/>.</returns>
 		/// <param name="mailbox">The mailbox.</param>
 		/// <exception cref="CertificateNotFoundException">
@@ -163,6 +211,13 @@ namespace UnitTests {
 		/// <summary>
 		/// Gets the <see cref="CmsSigner"/> for the specified mailbox.
 		/// </summary>
+		/// <remarks>
+		/// <para>Constructs a <see cref="CmsSigner"/> with the appropriate signing certificate
+		/// for the specified mailbox.</para>
+		/// <para>If the mailbox is a <see cref="SecureMailboxAddress"/>, the
+		/// <see cref="SecureMailboxAddress.Fingerprint"/> property will be used instead of
+		/// the mailbox address for database lookups.</para>
+		/// </remarks>
 		/// <returns>A <see cref="CmsSigner"/>.</returns>
 		/// <param name="mailbox">The mailbox.</param>
 		/// <param name="digestAlgo">The preferred digest algorithm.</param>
@@ -199,6 +254,9 @@ namespace UnitTests {
 		/// <summary>
 		/// Updates the known S/MIME capabilities of the client used by the recipient that owns the specified certificate.
 		/// </summary>
+		/// <remarks>
+		/// Updates the known S/MIME capabilities of the client used by the recipient that owns the specified certificate.
+		/// </remarks>
 		/// <param name="certificate">The certificate.</param>
 		/// <param name="algorithms">The encryption algorithm capabilities of the client (in preferred order).</param>
 		/// <param name="timestamp">The timestamp.</param>
@@ -208,47 +266,17 @@ namespace UnitTests {
 		}
 
 		/// <summary>
-		/// Import the specified certificate.
-		/// </summary>
-		/// <param name="certificate">The certificate.</param>
-		/// <exception cref="System.ArgumentNullException">
-		/// <paramref name="certificate"/> is <c>null</c>.
-		/// </exception>
-		public override void Import (X509Certificate certificate)
-		{
-			if (certificate == null)
-				throw new ArgumentNullException ("certificate");
-
-			certificates.Add (certificate);
-		}
-
-		/// <summary>
-		/// Import the specified certificate revocation list.
-		/// </summary>
-		/// <param name="crl">The certificate revocation list.</param>
-		/// <exception cref="System.ArgumentNullException">
-		/// <paramref name="crl"/> is <c>null</c>.
-		/// </exception>
-		public override void Import (X509Crl crl)
-		{
-			if (crl == null)
-				throw new ArgumentNullException ("crl");
-
-			crls.Add (crl);
-		}
-
-		/// <summary>
 		/// Imports certificates and keys from a pkcs12-encoded stream.
 		/// </summary>
+		/// <remarks>
+		/// Imports certificates and keys from a pkcs12-encoded stream.
+		/// </remarks>
 		/// <param name="stream">The raw certificate and key data.</param>
-		/// <param name="password">The password to unlock the data.</param>
+		/// <param name="password">The password to unlock the stream.</param>
 		/// <exception cref="System.ArgumentNullException">
 		/// <para><paramref name="stream"/> is <c>null</c>.</para>
 		/// <para>-or-</para>
 		/// <para><paramref name="password"/> is <c>null</c>.</para>
-		/// </exception>
-		/// <exception cref="System.NotSupportedException">
-		/// Importing keys is not supported by this cryptography context.
 		/// </exception>
 		public override void Import (Stream stream, string password)
 		{
@@ -274,6 +302,42 @@ namespace UnitTests {
 					certificates.Add (entry.Certificate);
 				}
 			}
+		}
+
+		/// <summary>
+		/// Imports the specified certificate.
+		/// </summary>
+		/// <remarks>
+		/// Imports the specified certificate.
+		/// </remarks>
+		/// <param name="certificate">The certificate.</param>
+		/// <exception cref="System.ArgumentNullException">
+		/// <paramref name="certificate"/> is <c>null</c>.
+		/// </exception>
+		public override void Import (X509Certificate certificate)
+		{
+			if (certificate == null)
+				throw new ArgumentNullException ("certificate");
+
+			certificates.Add (certificate);
+		}
+
+		/// <summary>
+		/// Imports the specified certificate revocation list.
+		/// </summary>
+		/// <remarks>
+		/// Imports the specified certificate revocation list.
+		/// </remarks>
+		/// <param name="crl">The certificate revocation list.</param>
+		/// <exception cref="System.ArgumentNullException">
+		/// <paramref name="crl"/> is <c>null</c>.
+		/// </exception>
+		public override void Import (X509Crl crl)
+		{
+			if (crl == null)
+				throw new ArgumentNullException ("crl");
+
+			crls.Add (crl);
 		}
 
 		#endregion
