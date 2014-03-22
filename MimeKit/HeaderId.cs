e7effbb6acc8628b2798cfa2822821a513f9b110
Jeffrey Stedfast <jeff@xamarin.com>
2014-03-21 21:28:25 -0400

    Removed need for reflection in HeaderId.cs to make it more portable

--- MimeKit/HeaderId.cs
+++ MimeKit/HeaderId.cs
@@ -373,40 +373,18 @@ namespace MimeKit {
 		Unknown = -1
 	}
 
-	[AttributeUsage (AttributeTargets.Field)]
-	sealed class HeaderNameAttribute : Attribute
-	{
-		public HeaderNameAttribute (string name)
-		{
-			HeaderName = name;
-		}
-
-		public string HeaderName {
-			get; private set;
-		}
-	}
-
 	static class HeaderIdExtension
 	{
 		public static string ToHeaderName (this Enum value)
 		{
-			var name = value.ToString ();
-			var type = value.GetType ();
-			var field = type.GetField (name);
+			var builder = new StringBuilder (value);
 
-			var attrs = field.GetCustomAttributes (typeof (HeaderNameAttribute), true);
-			if (attrs == null || attrs.Length == 0) {
-				var builder = new StringBuilder (name);
-
-				for (int i = 2; i < builder.Length; i++) {
-					if (char.IsUpper (builder[i]))
-						builder.Insert (i++, '-');
-				}
-
-				return builder.ToString ();
+			for (int i = 2; i < builder.Length; i++) {
+				if (char.IsUpper (builder[i]))
+					builder.Insert (i++, '-');
 			}
 
-			return ((HeaderNameAttribute) attrs[0]).HeaderName;
+			return builder.ToString ();
 		}
 
 		public static HeaderId ToHeaderId (this string name)
