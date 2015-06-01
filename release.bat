39c436ada4f68de6c852a7bbfbd714438e2120f5
Jeffrey Stedfast <jeff@xamarin.com>
2015-06-01 07:17:55 -0400

    Fixed release.bat

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
-"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /p:Platform=AnyCPU /t:Rebuild MimeKitLite.sln
-"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /p:Platform=AnyCPU /t:Rebuild MimeKit.sln
+"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /p:Platform="Any CPU" /t:Rebuild MimeKitLite.sln
+"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /p:Platform="Any CPU" /t:Rebuild MimeKit.sln
