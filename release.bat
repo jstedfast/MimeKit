b13a1e9d5a2aefc38699a5390e7d25c7d5aa4d12
Jeffrey Stedfast <jeff@xamarin.com>
2015-06-01 07:15:21 -0400

    Added release.bat to make releases simpler

---
+++ release.bat
@@ -0,0 +1,2 @@
+"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /p:Platform=AnyCPU /t:Rebuild MimeKitLite.sln
+"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /p:Platform=AnyCPU /t:Rebuild MimeKit.sln
