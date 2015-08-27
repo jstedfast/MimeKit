c7b1c5c6f2c8e8d0a83a56b1a99fd6dc6fcffdd8
Jeffrey Stedfast <jeff@xamarin.com>
2015-08-27 14:32:15 -0400

    Fixed the build

--- MimeKit/MimeMessage.cs
+++ MimeKit/MimeMessage.cs
@@ -1566,7 +1566,7 @@ namespace MimeKit {
 			out DkimCanonicalizationAlgorithm bodyAlgorithm, out string d, out string s, out string q, out string[] headers, out string bh, out string b, out int maxLength)
 		{
 			bool containsFrom = false;
-			string v, a, c, h, l, i;
+			string v, a, c, h, l, id;
 
 			if (!parameters.TryGetValue ("v", out v))
 				throw new FormatException ("Malformed DKIM-Signature header: no version parameter detected.");
@@ -1586,14 +1586,14 @@ namespace MimeKit {
 			if (!parameters.TryGetValue ("d", out d))
 				throw new FormatException ("Malformed DKIM-Signature header: no domain parameter detected.");
 
-			if (parameters.TryGetValue ("i", out i)) {
+			if (parameters.TryGetValue ("i", out id)) {
 				string ident;
 				int at;
 
-				if ((at = i.LastIndexOf ('@')) == -1)
+				if ((at = id.LastIndexOf ('@')) == -1)
 					throw new FormatException ("Malformed DKIM-Signature header: no @ in the AUID value.");
 
-				ident = i.Substring (at + 1);
+				ident = id.Substring (at + 1);
 
 				if (!ident.Equals (d, StringComparison.OrdinalIgnoreCase) && !ident.EndsWith ("." + d, StringComparison.OrdinalIgnoreCase))
 					throw new FormatException ("Invalid DKIM-Signature header: the domain in the AUID does not match the domain parameter.");
