1c95cfcd585d61b23be02b5f2bc001fc19f733ea
Jeffrey Stedfast <jeff@xamarin.com>
2016-01-01 10:06:46 -0500

    Bumped copyright

--- MimeKit/Cryptography/NpgsqlCertificateDatabase.cs
+++ MimeKit/Cryptography/NpgsqlCertificateDatabase.cs
@@ -3,7 +3,7 @@
 //
 // Author: Federico Di Gregorio <fog@initd.org>
 //
-// Copyright (c) 2013-2015 Xamarin Inc. (www.xamarin.com)
+// Copyright (c) 2013-2016 Xamarin Inc. (www.xamarin.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
