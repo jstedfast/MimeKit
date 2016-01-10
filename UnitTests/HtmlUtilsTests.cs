99607bd4f8ee1d2358b244f9535ee8dce7e26f96
Jeffrey Stedfast <jeff@xamarin.com>
2016-01-10 11:34:50 -0500

    Fixed the namespace

--- UnitTests/HtmlUtilsTests.cs
+++ UnitTests/HtmlUtilsTests.cs
@@ -3,7 +3,7 @@
 //
 // Author: Jeffrey Stedfast <jeff@xamarin.com>
 //
-// Copyright (c) 2016 Xamarin Inc. (www.xamarin.com)
+// Copyright (c) 2013-2016 Xamarin Inc. (www.xamarin.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@ using System.IO;
 
 using NUnit.Framework;
 
-using HtmlKit;
+using MimeKit.Text;
 
 namespace UnitTests {
 	[TestFixture]
