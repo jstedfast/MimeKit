8860aee4a3bb0a00d185d9f90c919ef7c3ca69fe
Jeffrey Stedfast <jeff@xamarin.com>
2016-02-28 09:57:42 -0500

    Fixed UrlScanner to properly handle IPv6 literals in addrspecs

--- UnitTests/UrlScannerTests.cs
+++ UnitTests/UrlScannerTests.cs
@@ -34,8 +34,7 @@ namespace UnitTests {
 	{
 		UrlScanner scanner;
 
-		[TestFixtureSetUp]
-		public void Setup ()
+		public UrlScannerTests ()
 		{
 			scanner = new UrlScanner ();
 
@@ -92,6 +91,18 @@ namespace UnitTests {
 		public void TestComplexQuotedLocalpartAddrspec ()
 		{
 			TestUrlScanner ("This is some text with a \"quoted \\\"local\\\" part\"@example.com in the text...", "\"quoted \\\"local\\\" part\"@example.com");
+		}
+
+		[Test]
+		public void TestIPv4Addrspec ()
+		{
+			TestUrlScanner ("This is some text with a ipv4@[127.0.0.1] in the text...", "ipv4@[127.0.0.1]");
+		}
+
+		[Test]
+		public void TestIPv6Addrspec ()
+		{
+			TestUrlScanner ("This is some text with a ipv6@[IPv6:::1] in the text...", "ipv6@[IPv6:::1]");
 		}
 
 		[Test]
