0e01c9f91f786272a7d9f159600b7cf410c16333
Jeffrey Stedfast <jeff@xamarin.com>
2016-05-02 17:19:42 -0400

    Set ComVisible to true

--- MimeKit/Properties/AssemblyInfo.cs
+++ MimeKit/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible (false)]
+[assembly: ComVisible (true)]
 
 #if !PORTABLE
 // The following GUID is for the ID of the typelib if this project is exposed to COM
