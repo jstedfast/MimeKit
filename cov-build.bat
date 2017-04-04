3d953accd3e44c80d4808c33bcbb7c38022ed0eb
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-04-04 07:56:47 -0400

    Added MimeKit.Coverity.sln
    This is to avoid Coverity scanning Mono.Data.SQLite and
    the UnitTests.

--- cov-build.bat
+++ cov-build.bat
@@ -1,1 +1,1 @@
-..\cov-analysis\bin\cov-build.exe --dir cov-int "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /t:Rebuild MimeKit.Net45.sln
+..\cov-analysis\bin\cov-build.exe --dir cov-int "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /t:Rebuild MimeKit.Coverity.sln
