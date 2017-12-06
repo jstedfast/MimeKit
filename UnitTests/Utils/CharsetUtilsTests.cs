0ba0b9f1a289b9cf87cd35f28a55495315bdea4f
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-12-05 22:22:44 -0500

    Added more unit tests for CharsetUtils

--- UnitTests/Utils/CharsetUtilsTests.cs
+++ UnitTests/Utils/CharsetUtilsTests.cs
@@ -26,6 +26,7 @@
 
 using System;
 using System.Text;
+using System.Security.Cryptography;
 
 using NUnit.Framework;
 
@@ -57,6 +58,9 @@ namespace UnitTests.Utils {
 			Assert.Throws<ArgumentNullException> (() => CharsetUtils.GetEncoding ("charset", null));
 			Assert.Throws<ArgumentOutOfRangeException> (() => CharsetUtils.GetEncoding (-1, "fallback"));
 			Assert.Throws<ArgumentNullException> (() => CharsetUtils.GetEncoding (28591, null));
+
+			Assert.Throws<ArgumentNullException> (() => CharsetUtils.GetMimeCharset ((string) null));
+			Assert.Throws<ArgumentNullException> (() => CharsetUtils.GetMimeCharset ((Encoding) null));
 		}
 
 		[Test]
@@ -82,7 +86,61 @@ namespace UnitTests.Utils {
 
 			Assert.AreEqual (1252, CharsetUtils.ParseCodePage ("windows-cp1252"));
 			Assert.AreEqual (1252, CharsetUtils.ParseCodePage ("windows-1252"));
+			Assert.AreEqual (1252, CharsetUtils.ParseCodePage ("cp-1252"));
 			Assert.AreEqual (1252, CharsetUtils.ParseCodePage ("cp1252"));
+
+			Assert.AreEqual (-1, CharsetUtils.ParseCodePage ("cp"));
+			Assert.AreEqual (-1, CharsetUtils.ParseCodePage ("iso"));
+			Assert.AreEqual (-1, CharsetUtils.ParseCodePage ("ibm"));
+			Assert.AreEqual (-1, CharsetUtils.ParseCodePage ("windows"));
+			Assert.AreEqual (-1, CharsetUtils.ParseCodePage ("windows-"));
+
+			Assert.AreEqual (-1, CharsetUtils.ParseCodePage ("iso-8859"));
+			Assert.AreEqual (-1, CharsetUtils.ParseCodePage ("iso-BB59"));
+			Assert.AreEqual (-1, CharsetUtils.ParseCodePage ("iso-8859-A"));
+			Assert.AreEqual (-1, CharsetUtils.ParseCodePage ("iso-2022-US"));
+			Assert.AreEqual (-1, CharsetUtils.ParseCodePage ("iso-4999-1"));
+		}
+
+		[Test]
+		public void TestConvertToUnicode ()
+		{
+			const string expected = "要連両存使破薫聞載線弁明小設幸名代開告覧。胸問中写視映的収掲笛事来更知。会教握話整団負画断問囲士英高枠営言。近着開交識営害新緑提犯趣大第快者一代田。海補間山間府序打面真教義優決位。需著会容同警士趣場社主交干今兆。電増罪合三時株再姿県人生。広暮分昭熊勢戦帯票昨切議読権月期春著。生上呼警上際岡作朝米趙情。";
+			var options = ParserOptions.Default.Clone ();
+			var gb2312 = Encoding.GetEncoding (936);
+
+			options.CharsetEncoding = gb2312;
+
+			var input = gb2312.GetBytes (expected);
+
+			var actual = CharsetUtils.ConvertToUnicode (options, input, 0, input.Length);
+
+			Assert.AreEqual (expected, actual);
+		}
+
+		[Test]
+		public void TestGetMimeCharset ()
+		{
+			Encoding encoding;
+
+			Assert.AreEqual ("iso-8859-1", CharsetUtils.GetMimeCharset ("latin1"));
+			Assert.AreEqual ("iso-8859-1", CharsetUtils.GetMimeCharset (CharsetUtils.Latin1));
+			Assert.AreEqual ("gibberish", CharsetUtils.GetMimeCharset ("gibberish"));
+
+			Assert.AreEqual ("iso-2022-jp", CharsetUtils.GetMimeCharset (Encoding.GetEncoding (932)));
+			Assert.AreEqual ("iso-2022-jp", CharsetUtils.GetMimeCharset (Encoding.GetEncoding (50220)));
+			Assert.AreEqual ("iso-2022-jp", CharsetUtils.GetMimeCharset (Encoding.GetEncoding (50221)));
+
+			try {
+				encoding = Encoding.GetEncoding (50225);
+			} catch (NotSupportedException) {
+				encoding = null;
+			}
+
+			if (encoding != null)
+				Assert.AreEqual ("euc-kr", CharsetUtils.GetMimeCharset (encoding));
+
+			Assert.AreEqual ("euc-kr", CharsetUtils.GetMimeCharset (Encoding.GetEncoding (949)));
 		}
 	}
 }
