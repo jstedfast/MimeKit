e5d966ac9ead325d84b73288f87934e2596c9469
Jeffrey Stedfast <jestedfa@microsoft.com>
2018-12-19 16:41:43 -0500

    Added some unit tests for X509CertificateRecord

---
+++ UnitTests/Cryptography/X509CertificateRecordTests.cs
@@ -0,0 +1,59 @@
+﻿//
+// X509CertificateRecordTests.cs
+//
+// Author: Jeffrey Stedfast <jestedfa@microsoft.com>
+//
+// Copyright (c) 2013-2017 Xamarin Inc. (www.xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+using System;
+using System.IO;
+
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.OpenSsl;
+
+using NUnit.Framework;
+
+using MimeKit.Cryptography;
+
+namespace UnitTests.Cryptography {
+	[TestFixture]
+	public class X509CertificateRecordTests
+	{
+		[Test]
+		public void TestArgumentExceptions ()
+		{
+			var signer = new CmsSigner (Path.Combine ("..", "..", "TestData", "smime", "smime.p12"), "no.secret");
+			AsymmetricCipherKeyPair keyPair;
+
+			using (var stream = new StreamReader (Path.Combine ("..", "..", "TestData", "dkim", "example.pem"))) {
+				var reader = new PemReader (stream);
+
+				keyPair = reader.ReadObject () as AsymmetricCipherKeyPair;
+			}
+
+			Assert.Throws<ArgumentNullException> (() => new X509CertificateRecord (null));
+			Assert.Throws<ArgumentNullException> (() => new X509CertificateRecord (null, keyPair.Private));
+			Assert.Throws<ArgumentNullException> (() => new X509CertificateRecord (signer.Certificate, null));
+			Assert.Throws<ArgumentException> (() => new X509CertificateRecord (signer.Certificate, keyPair.Public));
+		}
+	}
+}
