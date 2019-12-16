b26c918af7cdefecdd1882efc29cf0277efec75c
Jeffrey Stedfast <jestedfa@microsoft.com>
2019-12-16 10:17:34 -0500

    Added unit tests for RsaEncryptionPadding

---
+++ UnitTests/Cryptography/RsaEncryptionPaddingTests.cs
@@ -0,0 +1,128 @@
+﻿//
+// RsaEncryptionPaddingTests.cs
+//
+// Author: Jeffrey Stedfast <jestedfa@microsoft.com>
+//
+// Copyright (c) 2013-2019 Xamarin Inc. (www.xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Org.BouncyCastle.Asn1.Cms;
+using Org.BouncyCastle.Asn1.Oiw;
+using Org.BouncyCastle.Asn1.Nist;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.X509;
+
+using MimeKit.Cryptography;
+using Org.BouncyCastle.Asn1;
+
+namespace UnitTests.Cryptography {
+	[TestFixture]
+	public class RsaEncryptionPaddingTests
+	{
+		[Test]
+		public void TestEquality ()
+		{
+			Assert.AreEqual (RsaEncryptionPadding.OaepSha1, RsaEncryptionPadding.CreateOaep (DigestAlgorithm.Sha1), "SHA-1");
+			Assert.AreEqual (RsaEncryptionPadding.OaepSha256, RsaEncryptionPadding.CreateOaep (DigestAlgorithm.Sha256), "SHA-256");
+			Assert.AreEqual (RsaEncryptionPadding.OaepSha384, RsaEncryptionPadding.CreateOaep (DigestAlgorithm.Sha384), "SHA-384");
+			Assert.AreEqual (RsaEncryptionPadding.OaepSha512, RsaEncryptionPadding.CreateOaep (DigestAlgorithm.Sha512), "SHA-512");
+
+			Assert.AreNotEqual (RsaEncryptionPadding.Pkcs1, RsaEncryptionPadding.OaepSha1, "PKCS1 != SHA-1");
+			Assert.AreNotEqual (RsaEncryptionPadding.Pkcs1, RsaEncryptionPadding.OaepSha256, "PKCS1 != SHA-256");
+			Assert.AreNotEqual (RsaEncryptionPadding.OaepSha1, RsaEncryptionPadding.OaepSha256, "SHA-1 != SHA-256");
+
+			Assert.AreNotEqual (RsaEncryptionPadding.Pkcs1, new object (), "PKCS1 != object");
+		}
+
+		[Test]
+		public void TestGetHashCode ()
+		{
+			var hashCodes = new Dictionary<int, RsaEncryptionPadding> ();
+
+			foreach (var field in typeof (RsaEncryptionPadding).GetFields (BindingFlags.Public | BindingFlags.Static)) {
+				if (field.FieldType != typeof (RsaEncryptionPadding))
+					continue;
+
+				var padding = (RsaEncryptionPadding) field.GetValue (null);
+				int hashCode = padding.GetHashCode ();
+
+				if (hashCodes.TryGetValue (hashCode, out var other))
+					Assert.Fail ($"{padding.Scheme} shares the same hash code as {other.Scheme}");
+
+				hashCodes.Add (hashCode, padding);
+			}
+		}
+
+		[Test]
+		public void TestNotSupportedException ()
+		{
+			var supported = new HashSet<DigestAlgorithm> ();
+
+			foreach (var field in typeof (RsaEncryptionPadding).GetFields (BindingFlags.Public | BindingFlags.Static)) {
+				if (field.FieldType != typeof (RsaEncryptionPadding))
+					continue;
+
+				var padding = (RsaEncryptionPadding) field.GetValue (null);
+
+				if (padding.Scheme == RsaEncryptionPaddingScheme.Oaep)
+					supported.Add (padding.OaepHashAlgorithm);
+			}
+
+			foreach (DigestAlgorithm hashAlgorithm in Enum.GetValues (typeof (DigestAlgorithm))) {
+				if (!supported.Contains (hashAlgorithm))
+					Assert.Throws<NotSupportedException> (() => RsaEncryptionPadding.CreateOaep (hashAlgorithm));
+				else
+					Assert.DoesNotThrow (() => RsaEncryptionPadding.CreateOaep (hashAlgorithm));
+			}
+		}
+
+		static void AssertOaepAlgorithmIdentifier (RsaEncryptionPadding padding, DerObjectIdentifier hashAlgorithm)
+		{
+			var name = $"Oaep{padding.Scheme}";
+
+			var algorithm = padding.GetAlgorithmIdentifier ();
+			Assert.IsNotNull (algorithm, $"{name} != null");
+			Assert.AreEqual (PkcsObjectIdentifiers.IdRsaesOaep, algorithm.Algorithm, $"{name}.Algorithm == RSAES-OAEP");
+			var parameters = (RsaesOaepParameters) algorithm.Parameters;
+			Assert.AreEqual (hashAlgorithm, parameters.HashAlgorithm.Algorithm, $"{name}.HashAlgorithm == {padding.OaepHashAlgorithm}");
+			Assert.AreEqual (PkcsObjectIdentifiers.IdMgf1, parameters.MaskGenAlgorithm.Algorithm, $"{name}.MaskGenAlgorithm == MGF1");
+			var mgf1hash = (AlgorithmIdentifier) parameters.MaskGenAlgorithm.Parameters;
+			Assert.AreEqual (hashAlgorithm, mgf1hash.Algorithm, $"{name}.MaskGenHashAlgorithm == {padding.OaepHashAlgorithm}");
+		}
+
+		[Test]
+		public void TestGetAlgorithmIdentifier ()
+		{
+			Assert.IsNull (RsaEncryptionPadding.Pkcs1.GetAlgorithmIdentifier (), "Pkcs1");
+
+			AssertOaepAlgorithmIdentifier (RsaEncryptionPadding.OaepSha1, OiwObjectIdentifiers.IdSha1);
+			AssertOaepAlgorithmIdentifier (RsaEncryptionPadding.OaepSha256, NistObjectIdentifiers.IdSha256);
+			AssertOaepAlgorithmIdentifier (RsaEncryptionPadding.OaepSha384, NistObjectIdentifiers.IdSha384);
+			AssertOaepAlgorithmIdentifier (RsaEncryptionPadding.OaepSha512, NistObjectIdentifiers.IdSha512);
+		}
+	}
+}
