e17bb1fa019fbd6f1435a3e74f9023962ad92cef
Jeffrey Stedfast <jestedfa@microsoft.com>
2020-06-17 20:57:53 -0400

    Update DkimPublicKeyLocatorBaseTests.cs

--- UnitTests/Cryptography/DkimPublicKeyLocatorBaseTests.cs
+++ UnitTests/Cryptography/DkimPublicKeyLocatorBaseTests.cs
@@ -29,6 +29,7 @@ using System;
 using NUnit.Framework;
 
 using MimeKit;
+using Org.BouncyCastle.Crypto.Parameters;
 
 namespace UnitTests.Cryptography {
 	[TestFixture]
@@ -53,16 +54,28 @@ namespace UnitTests.Cryptography {
 			locator.Add ("empty._domainkey.example.org", string.Empty);
 			locator.Add ("whitespace._domainkey.example.org", "     ");
 			locator.Add ("no-k-or-p-params._domainkey.example.org", "v=DKIM1; x=abc; y=def");
-			locator.Add ("no-k-param._domainkey.example.org", "v=DKIM1; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDkHlOQoBTzWRiGs5V6NpP3id Y6Wk08a5qhdR6wy5bdOKb2jLQiY/J16JYi0Qvx/byYzCNb3W91y3FutACDfzwQ/BC/e/8uBsCR+yz1Lx j+PL6lHvqMKrM3rG4hstT5QjvHO9PzoxZyVYLzBfO2EeC3Ip3G+2kryOTIKT+l/K4w3QIDAQAB");
+			//locator.Add ("no-k-param._domainkey.example.org", "v=DKIM1; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDkHlOQoBTzWRiGs5V6NpP3id Y6Wk08a5qhdR6wy5bdOKb2jLQiY/J16JYi0Qvx/byYzCNb3W91y3FutACDfzwQ/BC/e/8uBsCR+yz1Lx j+PL6lHvqMKrM3rG4hstT5QjvHO9PzoxZyVYLzBfO2EeC3Ip3G+2kryOTIKT+l/K4w3QIDAQAB");
 			locator.Add ("no-p-param._domainkey.example.org", "v=DKIM1; k=rsa");
 			locator.Add ("unknown-algorithm._domainkey.example.org", "v=DKIM1; k=dummy; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDkHlOQoBTzWRiGs5V6NpP3id");
 
 			Assert.Throws<ParseException> (() => locator.LocatePublicKey ("dns/txt", "example.org", "empty"));
 			Assert.Throws<ParseException> (() => locator.LocatePublicKey ("dns/txt", "example.org", "whitespace"));
 			Assert.Throws<ParseException> (() => locator.LocatePublicKey ("dns/txt", "example.org", "no-k-or-p-params"));
-			Assert.Throws<ParseException> (() => locator.LocatePublicKey ("dns/txt", "example.org", "no-k-param"));
+			//Assert.Throws<ParseException> (() => locator.LocatePublicKey ("dns/txt", "example.org", "no-k-param"));
 			Assert.Throws<ParseException> (() => locator.LocatePublicKey ("dns/txt", "example.org", "no-p-param"));
 			Assert.Throws<ParseException> (() => locator.LocatePublicKey ("dns/txt", "example.org", "unknown-algorithm"));
+		}
+
+		[Test]
+		public void TestParseMissingKParamDefaultsToRsa ()
+		{
+			var locator = new DkimPublicKeyLocator ();
+
+			locator.Add ("no-k-param._domainkey.example.org", "v=DKIM1; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDkHlOQoBTzWRiGs5V6NpP3id Y6Wk08a5qhdR6wy5bdOKb2jLQiY/J16JYi0Qvx/byYzCNb3W91y3FutACDfzwQ/BC/e/8uBsCR+yz1Lx j+PL6lHvqMKrM3rG4hstT5QjvHO9PzoxZyVYLzBfO2EeC3Ip3G+2kryOTIKT+l/K4w3QIDAQAB");
+
+			var key = locator.LocatePublicKey ("dns/txt", "example.org", "no-k-param");
+
+			Assert.IsInstanceOf<RsaKeyParameters> (key);
 		}
 	}
 }
