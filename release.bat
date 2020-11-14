af0736fc3fc30527dbcf4fe29efdb26a6a665748
Jeffrey Stedfast <jestedfa@microsoft.com>
2020-11-14 12:15:37 -0500

    Removed release.bat

--- release.bat
+++
@@ -1,2 +0,0 @@
-"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /p:Platform="Any CPU" /t:Rebuild MimeKitLite.sln
-"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /p:Platform="Any CPU" /t:Rebuild MimeKit.sln
