bb0ec5bed792375e1e258b98e3fffe847c1d45a5
Jeffrey Stedfast <jestedfa@microsoft.com>
2021-05-04 21:28:06 -0400

    Enable SqliteCertificateDatabase initialization logic for .NET v5.0
    Fixes issue #673

--- MimeKit/Cryptography/SqliteCertificateDatabase.cs
+++ MimeKit/Cryptography/SqliteCertificateDatabase.cs
@@ -99,7 +99,7 @@ namespace MimeKit.Cryptography {
 #if __MOBILE__
 			IsAvailable = true;
 #else // !__MOBILE__
-#if NETFRAMEWORK || NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP3_0
+#if NETFRAMEWORK || NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP3_0 || NET5_0_OR_GREATER
 			var platform = Environment.OSVersion.Platform;
 #endif
 
@@ -113,7 +113,7 @@ namespace MimeKit.Cryptography {
 			}
 #endif
 
-#if NETFRAMEWORK || NETCOREAPP3_0
+#if NETFRAMEWORK || NETCOREAPP3_0 || NET5_0_OR_GREATER
 			// Mono.Data.Sqlite will only work on Unix-based platforms.
 			if (platform == PlatformID.Unix || platform == PlatformID.MacOSX) {
 				if ((sqliteAssembly = SQLiteAssembly.Load ("Mono.Data.Sqlite")) != null) {
@@ -126,7 +126,7 @@ namespace MimeKit.Cryptography {
 			}
 #endif
 
-#if NETFRAMEWORK || NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP3_0
+#if NETFRAMEWORK || NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP3_0 || NET5_0_OR_GREATER
 			if ((sqliteAssembly = SQLiteAssembly.Load ("System.Data.SQLite")) != null) {
 				// Make sure that the runtime can load the native sqlite3 library
 				if (VerifySQLiteAssemblyIsUsable ()) {
