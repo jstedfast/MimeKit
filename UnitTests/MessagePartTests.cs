4966f14796897e03fcb763f5200746e4dc414687
Jeffrey Stedfast <jestedfa@microsoft.com>
2021-06-26 12:30:47 -0400

    Added unit tests for MessagePart

---
+++ UnitTests/MessagePartTests.cs
@@ -0,0 +1,82 @@
+﻿//
+// MessagePartTests.cs
+//
+// Author: Jeffrey Stedfast <jestedfa@microsoft.com>
+//
+// Copyright (c) 2013-2021 .NET Foundation and Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using MimeKit;
+
+namespace UnitTests {
+	[TestFixture]
+	public class MessagePartTests
+	{
+		[Test]
+		public void TestPrepare ()
+		{
+			using (var content = new MemoryStream (new byte[64], false)) {
+				var part = new MimePart ("application/octet-stream") {
+					Content = new MimeContent (content)
+				};
+				var message = new MimeMessage () {
+					Body = part
+				};
+				var rfc822 = new MessagePart () {
+					Message = message
+				};
+
+				var encoding = part.GetBestEncoding (EncodingConstraint.SevenBit);
+
+				Assert.AreEqual (ContentEncoding.Base64, encoding, "GetBestEncoding");
+
+				rfc822.Prepare (EncodingConstraint.SevenBit);
+
+				Assert.AreEqual (ContentEncoding.Base64, part.ContentTransferEncoding, "Prepare #1");
+
+				// now make sure that calling Prepare() again doesn't change anything
+
+				rfc822.Prepare (EncodingConstraint.SevenBit);
+
+				Assert.AreEqual (ContentEncoding.Base64, part.ContentTransferEncoding, "Prepare #2");
+
+				part.ContentTransferEncoding = ContentEncoding.Binary;
+				rfc822.Prepare (EncodingConstraint.None);
+
+				Assert.AreEqual (ContentEncoding.Binary, part.ContentTransferEncoding, "Prepare #3");
+
+				rfc822.Prepare (EncodingConstraint.SevenBit);
+
+				Assert.AreEqual (ContentEncoding.Base64, part.ContentTransferEncoding, "Prepare #4");
+			}
+		}
+	}
+}
