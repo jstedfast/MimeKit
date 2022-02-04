e17d85fb2c7dae041a2a07b585f54eccbca5120f
Jason Nelson <jason@carbonmade.com>
2022-02-04 13:01:07 -0800

    Add polyfill for StringBuilder.Append(ReadOnlySpan<char>)

---
+++ MimeKit/Polyfills/StringBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿#if NETSTANDARD2_0 || NETFRAMEWORK
+
+using System;
+using System.Text;
+
+namespace MimeKit;
+
+internal static class StringBuilderExtensions
+{
+	public static void Append (this StringBuilder sb, ReadOnlySpan<char> value)
+	{
+		sb.Append (value.ToString ());
+	}
+}
+#endif
