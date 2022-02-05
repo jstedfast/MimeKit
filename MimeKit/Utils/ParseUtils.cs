jstedfast/MimeKit

commit d07caf3ba14267890e93f910d6ddaaaeead3362d

More reduction of string allocations using Span and other techniques

MimeKit/Utils/ParseUtils.cs
@@ -519,14 +519,26 @@ namespace MimeKit.Utils {
 			return true;
 		}
 
-		public static bool IsInternational (string value)
+		public static bool IsInternational (string value, int startIndex, int count)
 		{
-			for (int i = 0; i < value.Length; i++) {
+			int endIndex = startIndex + count;
+
+			for (int i = startIndex; i < endIndex; i++) {
 				if (value[i] > 127)
 					return true;
 			}
 
 			return false;
+		}
+
+		public static bool IsInternational (string value, int startIndex)
+		{
+			return IsInternational (value, startIndex, value.Length - startIndex);
+		}
+
+		public static bool IsInternational (string value)
+		{
+			return IsInternational (value, 0, value.Length);
 		}
 
 		public static bool IsIdnEncoded (string value)
@@ -535,6 +547,24 @@ namespace MimeKit.Utils {
 				return true;
 
 			return value.IndexOf (".xn--", StringComparison.Ordinal) != -1;
+		}
+
+		public static string IdnEncode (string unicode, int startIndex, int count)
+		{
+			try {
+				return idn.GetAscii (unicode, startIndex, count);
+			} catch {
+				return unicode.Substring (0, count);
+			}
+		}
+
+		public static string IdnEncode (string unicode, int startIndex)
+		{
+			try {
+				return idn.GetAscii (unicode, startIndex);
+			} catch {
+				return unicode.Substring (startIndex);
+			}
 		}
 
 		public static string IdnEncode (string unicode)

MimeKit/Cryptography/DkimVerifierBase.cs
@@ -184,7 +184,7 @@ namespace MimeKit.Cryptography {
 				if (index >= signature.Length)
 					continue;
 
-				var name = signature.Substring (startIndex, index - startIndex).TrimEnd ();
+				var name = signature.AsSpan (startIndex, index - startIndex).TrimEnd ().ToString ();
 
 				// skip over '=' and clear value buffer
 				value.Length = 0;

MimeKit/InternetAddress.cs
@@ -164,10 +164,15 @@ namespace MimeKit {
 				}
 
 				if (rv == 0) {
-					string otherUser = otherAt != -1 ? otherAddress.Substring (0, otherAt) : otherAddress;
-					string user = at != -1 ? address.Substring (0, at) : address;
+					int otherLength = otherAt == -1 ? otherAddress.Length : otherAt;
+					int length = at == -1 ? address.Length : at;
+					int n = Math.Min (length, otherLength);
 
-					rv = string.Compare (user, otherUser, StringComparison.OrdinalIgnoreCase);
+					if ((rv = string.Compare (address, 0, otherAddress, 0, n, StringComparison.OrdinalIgnoreCase)) == 0) {
+						// The local-part's of the addresses are identical for the first `n` characters. The address
+						// with the longer local-part should sort as > the address with the shorter local-part.
+						rv = length - otherLength;
+					}
 				}
 
 				return rv;

MimeKit/MailboxAddress.cs
@@ -268,14 +268,17 @@ namespace MimeKit {
 		static string EncodeAddrspec (string addrspec, int at)
 		{
 			if (at != -1) {
-				var domain = addrspec.Substring (at + 1);
-				var local = addrspec.Substring (0, at);
+				string local, domain;
 
-				if (ParseUtils.IsInternational (local))
-					local = ParseUtils.IdnEncode (local);
+				if (ParseUtils.IsInternational (addrspec, 0, at))
+					local = ParseUtils.IdnEncode (addrspec, 0, at);
+				else
+					local = addrspec.Substring (0, at);
 
-				if (ParseUtils.IsInternational (domain))
-					domain = ParseUtils.IdnEncode (domain);
+				if (ParseUtils.IsInternational (addrspec, at + 1))
+					domain = ParseUtils.IdnEncode (addrspec, at + 1);
+				else
+					domain = addrspec.Substring (at + 1);
 
 				return local + "@" + domain;
 			}

MimeKit/Text/FlowedToHtml.cs
@@ -29,6 +29,8 @@ using System.IO;
 using System.Text;
 using System.Collections.Generic;
 
+using MimeKit.Utils;
+
 namespace MimeKit.Text {
 	/// <summary>
 	/// A flowed text to HTML converter.
@@ -186,7 +188,7 @@ namespace MimeKit.Text {
 			tagContext.WriteTag (htmlWriter, true);
 		}
 
-		static string Unquote (string line, out int quoteDepth)
+		static ReadOnlySpan<char> Unquote (ReadOnlySpan<char> line, out int quoteDepth)
 		{
 			int index = 0;
 
@@ -203,7 +205,7 @@ namespace MimeKit.Text {
 			if (index > 0 && index < line.Length && line[index] == ' ')
 				index++;
 
-			return index > 0 ? line.Substring (index) : line;
+			return index > 0 ? line.Slice (index) : line;
 		}
 
 		static bool SuppressContent (IList<FlowedToHtmlTagContext> stack)
@@ -360,11 +362,11 @@ namespace MimeKit.Text {
 
 				while ((line = reader.ReadLine ()) != null) {
 					// unquote the line
-					line = Unquote (line, out int quoteDepth);
+					var unquoted = Unquote (line.AsSpan (), out int quoteDepth);
 
-					// remove space-stuffing
-					if (line.Length > 0 && line[0] == ' ')
-						line = line.Substring (1);
+					// if there is a leading space, it was stuffed
+					if (quoteDepth == 0 && unquoted.Length > 0 && unquoted[0] == ' ')
+						unquoted = unquoted.Slice (1);
 
 					if (para.Length == 0) {
 						paraQuoteDepth = quoteDepth;
@@ -377,9 +379,9 @@ namespace MimeKit.Text {
 						para.Length = 0;
 					}
 
-					para.Append (line);
+					para.Append (unquoted);
 
-					if (line.Length == 0 || line[line.Length - 1] != ' ') {
+					if (unquoted.Length == 0 || unquoted[unquoted.Length - 1] != ' ') {
 						// line did not end with a space, so the next line will start a new paragraph
 						WriteParagraph (htmlWriter, stack, ref currentQuoteDepth, para, paraQuoteDepth);
 						paraQuoteDepth = 0;

UnitTests/InternetAddressListTests.cs
@@ -809,7 +809,19 @@ namespace UnitTests {
 			Assert.IsTrue (mailbox.CompareTo (group) < 0, "CompareTo() should return < 0.");
 			Assert.IsTrue (group.CompareTo (mailbox) > 0, "CompareTo() should return > 0.");
 
-			Assert.IsTrue (mailbox.CompareTo (group.Members[0]) == 0, "CompareTo() should return 0.");
+			Assert.AreEqual (0, mailbox.CompareTo (group.Members[0]), "CompareTo() should return 0.");
+
+			var alice = new MailboxAddress (string.Empty, "alice@example.com");
+			var bob = new MailboxAddress (string.Empty, "bob@example.com");
+
+			Assert.IsTrue (alice.CompareTo (bob) < 0, "alice.CompareTo(bob) should return < 0.");
+			Assert.IsTrue (bob.CompareTo (alice) > 0, "bob.CompareTo(alice) should return > 0.");
+
+			var alexa = new MailboxAddress (string.Empty, "alexa@example.com");
+			var alex = new MailboxAddress (string.Empty, "alex@example.com");
+
+			Assert.IsTrue (alex.CompareTo (alexa) < 0, "alex.CompareTo(alexa) should return < 0.");
+			Assert.IsTrue (alexa.CompareTo (alex) > 0, "alexa.CompareTo(alex) should return > 0.");
 		}
 
 		#region Rfc7103

commit ddd1121da3227b681af8f56d27ad898b1bdb240e

Fixed bug introduced in commit d07caf3ba14267890e93f910d6ddaaaeead3362d

MimeKit/Utils/ParseUtils.cs
@@ -554,7 +554,7 @@ namespace MimeKit.Utils {
 			try {
 				return idn.GetAscii (unicode, startIndex, count);
 			} catch {
-				return unicode.Substring (0, count);
+				return unicode.Substring (startIndex, count);
 			}
 		}
 