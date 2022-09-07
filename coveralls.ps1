79e81aba816424f9c662b13b64988e1edb0ed019
Jeffrey Stedfast <jestedfa@microsoft.com>
2022-09-07 14:42:19 -0400

    Upated coveralls.ps1

--- coveralls.ps1
+++ coveralls.ps1
@@ -23,15 +23,6 @@ $OpenCoverProfiler32 = Join-Path $openCoverToolsDir "x86\OpenCover.Profiler.dll"
 $OpenCoverProfiler64 = Join-Path $openCoverToolsDir "x64\OpenCover.Profiler.dll"
 $OpenCover = Join-Path $openCoverToolsDir "OpenCover.Console.exe"
 
-# Get the coveralls.net executable path
-$packageReference = $project.SelectSingleNode("/Project/ItemGroup/PackageReference[@Include='coveralls.net']")
-$coverallsVersion = $packageReference.GetAttribute("Version")
-$coverallsBasePackageDir = Join-Path $nugetPackagesDir "coveralls.net"
-$coverallsPackageDir = Join-Path $coverallsBasePackageDir $coverallsVersion
-$coverallsToolsDir = Join-Path $coverallsPackageDir "tools"
-
-$Coveralls = Join-Path $coverallsToolsDir "csmacnz.Coveralls.exe"
-
 # Get the OutputPath
 $targetFramework = $project.SelectSingleNode("/Project/PropertyGroup/TargetFramework")
 $OutputDir = Join-Path "UnitTests\bin\Debug" $targetFramework.InnerText
@@ -46,13 +37,37 @@ $OutputDir = Join-Path "UnitTests\bin\Debug" $targetFramework.InnerText
 	-targetargs:"--domain:single UnitTests.dll" `
 	-output:opencover.xml
 
-& $Coveralls --opencover -i opencover.xml `
-	--repoToken $env:COVERALLS_REPO_TOKEN `
-	--useRelativePaths `
-	--basePath $OutputDir `
-	--commitId $env:GIT_COMMIT_SHA `
-	--commitBranch $env:GIT_REF.Replace('refs/heads/', '') `
-	--commitAuthor $env:GIT_ACTOR `
-	--commitEmail $env:GIT_ACTOR_EMAIL `
-	--commitMessage $env:GIT_COMMIT_MESSAGE `
-	--jobId $env:COVERALLS_JOB_ID
+# Get the coveralls.net executable path
+$dotnetToolsPath = Join-Path ".config" "dotnet-tools.json"
+if (Test-Path -Path $dotnetToolsPath -PathType Leaf) {
+	& dotnet tool run csmacnz.Coveralls --opencover -i opencover.xml `
+		--repoToken $env:COVERALLS_REPO_TOKEN `
+		--useRelativePaths `
+		--basePath $OutputDir `
+		--commitId $env:GIT_COMMIT_SHA `
+		--commitBranch $env:GIT_REF.Replace('refs/heads/', '') `
+		--commitAuthor $env:GIT_ACTOR `
+		--commitEmail $env:GIT_ACTOR_EMAIL `
+		--commitMessage $env:GIT_COMMIT_MESSAGE `
+		--jobId $env:COVERALLS_JOB_ID
+} else {
+	$packageReference = $project.SelectSingleNode("/Project/ItemGroup/PackageReference[@Include='coveralls.net']")
+	$coverallsVersion = $packageReference.GetAttribute("Version")
+	$coverallsBasePackageDir = Join-Path $nugetPackagesDir "coveralls.net"
+	$coverallsPackageDir = Join-Path $coverallsBasePackageDir $coverallsVersion
+	$coverallsToolsDir = Join-Path $coverallsPackageDir "tools"
+
+	$Coveralls = Join-Path $coverallsToolsDir "csmacnz.Coveralls.exe"
+
+	& $Coveralls --opencover -i opencover.xml `
+		--repoToken $env:COVERALLS_REPO_TOKEN `
+		--useRelativePaths `
+		--basePath $OutputDir `
+		--commitId $env:GIT_COMMIT_SHA `
+		--commitBranch $env:GIT_REF.Replace('refs/heads/', '') `
+		--commitAuthor $env:GIT_ACTOR `
+		--commitEmail $env:GIT_ACTOR_EMAIL `
+		--commitMessage $env:GIT_COMMIT_MESSAGE `
+		--jobId $env:COVERALLS_JOB_ID
+}
+
