3928b53b70bc803f910b1a76690f6162b3986322
Jeffrey Stedfast <jestedfa@microsoft.com>
2023-09-01 10:27:19 -0400

    Switch from OpenCover to AltCover

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -11,53 +11,12 @@ Write-Output "Configuration:        $Configuration"
 Write-Output "GenerateCodeCoverage: $GenerateCodeCoverage"
 Write-Output ""
 
-[xml]$project = Get-Content UnitTests\UnitTests.csproj
-
-$nugetPackagesDir = Join-Path $Home ".nuget" "packages"
-
-# Get the NUnit.ConsoleRunner executable path
-$packageReference = $project.SelectSingleNode("/Project/ItemGroup/PackageReference[@Include='NUnit.ConsoleRunner']")
-$consoleRunnerVersion = $packageReference.GetAttribute("Version")
-
-$NUnitConsoleRunner = Join-Path $nugetPackagesDir "nunit.consolerunner" $consoleRunnerVersion "tools" "nunit3-console.exe"
-
-# Get the OutputPath
-$targetFramework = $project.SelectSingleNode("/Project/PropertyGroup/TargetFramework")
-$OutputDir = Join-Path "UnitTests" "bin" $Configuration $targetFramework.InnerText
-$UnitTestsAssembly = Join-Path $OutputDir "UnitTests.dll"
-
 if ($GenerateCodeCoverage -eq 'yes') {
-    # Get the OpenCover executable path
-    $packageReference = $project.SelectSingleNode("/Project/ItemGroup/PackageReference[@Include='OpenCover']")
-    $openCoverVersion = $packageReference.GetAttribute("Version")
-    $openCoverToolsDir = Join-Path $nugetPackagesDir "opencover" $openCoverVersion "tools"
-
-    $OpenCoverProfiler32 = Join-Path $openCoverToolsDir "x86" "OpenCover.Profiler.dll"
-    $OpenCoverProfiler64 = Join-Path $openCoverToolsDir "x64" "OpenCover.Profiler.dll"
-    $OpenCover = Join-Path $openCoverToolsDir "OpenCover.Console.exe"
-
-    try {
-        & regsvr32 $OpenCoverProfiler32
-    } catch {
-        Write-Output "Failed to register $OpenCoverProfiler32"
-    }
-
-    try {
-        & regsvr32 $OpenCoverProfiler64
-    } catch {
-        Write-Output "Failed to register $OpenCoverProfiler64"
-    }
-
     Write-Output "Running the UnitTests (code coverage enabled)"
 
-    # Run OpenCover
-    & $OpenCover -filter:"+[MimeKit]* -[UnitTests]*" `
-        -target:"$NUnitConsoleRunner" `
-        -targetargs:"--domain:single $UnitTestsAssembly" `
-        -output:opencover.xml
+    dotnet test -v 5 /p:Configuration=$Configuration /p:AltCover=true /p:AltCoverAssemblyExcludeFilter="Microsoft*" /p:AltCoverAssemblyExcludeFilter="NUnit*" /p:AltCoverAssemblyExcludeFilter="UnitTests*" UnitTests\UnitTests.csproj
 } else {
     Write-Output "Running the UnitTests"
 
-    # Run OpenCover
-    & $NUnitConsoleRunner --domain:single $UnitTestsAssembly
+    dotnet test -v 5 /p:Configuration=$Configuration UnitTests\UnitTests.csproj
 }
